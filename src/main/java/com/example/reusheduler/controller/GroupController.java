330d992abdddc89a1fa28d7cff69ce9909ddf50f
Max Mikhailov <mikhai1ovMax@yandex.ru>
2024-05-22 22:58:40 +0300

    123

--- src/main/java/com/example/reusheduler/controller/GroupController.java
+++ src/main/java/com/example/reusheduler/controller/GroupController.java
@@ -2,11 +2,15 @@ package com.example.reusheduler.controller;
 
 import com.example.reusheduler.model.Group;
 import com.example.reusheduler.repository.GroupRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @RestController
 public class GroupController {
@@ -24,4 +28,24 @@ public class GroupController {
 
         return groupList;
     }
+
+    @PostMapping("/fill/group")
+    public ResponseEntity<Object> objectResponseEntity(@RequestParam Long groupNumber,
+                                                       @RequestParam String direction){
+        if(groupRepository.existsByGroupNumber(groupNumber))
+            return ResponseEntity.badRequest().build();
+        else {
+            Group groupDB = groupRepository.findFirstByOrderByIdDesc();
+
+            Group group = Group.builder()
+                    .id(groupDB.getId()+1)
+                    .groupNumber(groupNumber)
+                    .direction(direction)
+                    .build();
+            groupRepository.save(group);
+
+            return ResponseEntity.ok().build();
+        }
+    }
+
 }
--- src/main/java/com/example/reusheduler/repository/GroupRepository.java
+++ src/main/java/com/example/reusheduler/repository/GroupRepository.java
@@ -1,10 +1,15 @@
 package com.example.reusheduler.repository;
 
 import com.example.reusheduler.model.Curriculum;
+import com.example.reusheduler.model.Department;
 import com.example.reusheduler.model.Group;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface GroupRepository extends CrudRepository<Group, Long> {
+    boolean existsByGroupNumber(Long groupNumber);
+
+    Group findFirstByOrderByIdDesc();
+
 }
