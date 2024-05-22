cdcbdcf2149724f45f66a3b9a3c0cc2b63ae4124
Max Mikhailov <mikhai1ovMax@yandex.ru>
2024-05-22 23:17:42 +0300

    123

--- src/main/java/com/example/reusheduler/controller/RaspController.java
+++ src/main/java/com/example/reusheduler/controller/RaspController.java
@@ -3,15 +3,28 @@ package com.example.reusheduler.controller;
 
 import com.example.reusheduler.dto.RaspDTO;
 import com.example.reusheduler.model.Schedule;
+import com.example.reusheduler.repository.ScheduleRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+
+//первый модуль (01.09 – 31.10),
+//второй модуль (01.11-31.12),
+//третий модуль (01.01 – 31.03),
+//четвертый модуль (01.04 – 31.06)
+
 @RestController
 public class RaspController {
 
-//    @GetMapping("view/rasp")
-//    public Schedule viewRasp(@RequestBody RaspDTO raspDTO){
-//
-//    }
+    private final ScheduleRepository scheduleRepository;
+
+    public RaspController(ScheduleRepository scheduleRepository) {
+        this.scheduleRepository = scheduleRepository;
+    }
+
+    @GetMapping("/view/rasp")
+    public Schedule viewRasp(@RequestBody RaspDTO raspDTO){
+
+    }
 }
--- src/main/java/com/example/reusheduler/dto/RaspDTO.java
+++ src/main/java/com/example/reusheduler/dto/RaspDTO.java
@@ -5,8 +5,18 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.ToString;
 
+import java.time.LocalDate;
+
 @Builder
 @ToString
 @Data
 public class RaspDTO {
+
+    private Long groupId;
+
+    private Long module;
+
+    private LocalDate startDate;
+
+    private LocalDate endDate;
 }
